3f6c10746b259659d8375f64d3a048a34975eb9c
monemail <monemail@cequevousvoulez.com>
2022-01-28 10:28:41 +0100

    Add server_start.sh

---
+++ server/server_start.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+script_name=$0
+ssh_status=$(dpkg -s ssh | grep "Status:")
+ssh_server_status=$(dpkg -s openssh-server | grep "Status:")
+
+function get_ssh_server_status {
+	if [ "$(service --status-all | grep ssh)" = " [ + ]  ssh" ]
+        then
+        	echo 1
+        elif [ "$(service --status-all | grep ssh)" = " [ - ]  ssh" ]
+        then
+            echo 0
+        else
+            echo -1
+        fi
+}
+
+function print_ssh_server_status {
+	if [ $(get_ssh_server_status) -eq 1 ]
+	then
+		echo "[✓] ssh server is on"
+	elif [ $(get_ssh_server_status) -eq 0 ]
+	then
+		echo "[✓] ssh server is off"
+	else
+		echo "$(service --status-all | grep ssh)"
+	fi
+
+	echo
+}
+
+if [ "$ssh_status" = "Status: install ok installed" ]
+then
+	echo "[✓] ssh"
+else
+	echo "[X] ssh"
+	echo "[installing] ssh"
+	sudo apt install ssh
+fi
+
+if [ "$ssh_server_status" = "Status: install ok installed" ]
+then
+    echo "[✓] openssh-server"
+else
+    echo "[X] openssh-server"
+    echo "[installing] openssh-server"
+    sudo apt install openssh-server
+fi
+
+echo
+
+if [ "$1" = "start" ]
+then
+	sudo systemctl start ssh
+	echo "[✓] serveur ssh démarré"
+	print_ssh_server_status
+	ip=$(ifconfig | grep "inet.*broadcast" | cut -c14-28)
+	echo "IP du serveur: $ip"
+    bash ./user_creation.sh create
+elif [ "$1" = "stop" ]
+then
+    sudo systemctl stop ssh
+    echo "[✓] serveur ssh éteint"
+    print_ssh_server_status
+    bash ./user_creation.sh remove
+else
+    echo "Argument manquant:"
+    echo "Ex:" $script_name "start"
+    exit 0
+fi
