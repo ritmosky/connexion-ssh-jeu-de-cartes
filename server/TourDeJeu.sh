b5ac95b6cf6ac8346349d192922c7af3a5c69aa9
Damien Vaurs <damien.vaurs@etu.utc.fr>
2022-01-28 14:47:13 +0100

    Nouvelle version de TourDeJeu.sh

--- server/TourDeJeu.sh
+++ server/TourDeJeu.sh
@@ -6,7 +6,58 @@ function jouerCarte {
     echo "$1 $2" >> etatJeu.txt
 }
 
+function scoreCarte {
+    carteJouee=$1
+    case ${carteJouee:1:1} in
 
+        ♣)
+        coeff=100
+        ;;
+
+        ♦)
+        coeff=125
+        ;;
+
+        ♥)
+        coeff=150
+        ;;
+
+        ♠)
+        coeff=175
+        ;;
+
+        *)
+        echo Erreur
+        ;;
+
+    esac
+
+    case ${carteJouee:0:1} in
+
+        J)
+        nombre=11
+        ;;
+
+        Q)
+        nombre=12
+        ;;
+
+        K)
+        nombre=13
+        ;;
+
+        A)
+        nombre=14
+        ;;
+
+        *)
+        nombre=${carteJouee:0:1}
+        ;;
+
+    esac
+
+    echo $((nombre*coeff))
+}
 
 function tourDeJeu {
     JoueurActuel=$1 
@@ -14,31 +65,32 @@ function tourDeJeu {
 
     ./AffichageJoueur.sh $JoueurActuel
 
-    cat /dev/null > temp/"${JoueurActuel}Out"
+    cat /dev/null > temp/${JoueurActuel}Out
 
     while [ ! $(cat temp/${JoueurActuel}Out) ] 
     do
         carte=$(cat temp/${JoueurActuel}Out)
     done
 
-    cat /dev/null > temp/"${JoueurActuel}Out"
+    cat /dev/null > temp/${JoueurActuel}Out
     sed '/$carte/d' "temp/${JoueurActuel}In" > "temp/${JoueurActuel}In"
+    carte=$(scoreCarte $carte)
     jouerCarte $joueurActuel $carte
-
-    
-
 
 }
 
+nbJoueurs=$(cat temp/nb_player.dat)
+declare -a score
+
 for i in {1..7}
 do
-    for joueur in $(ls listeJoueurs)
+    for j in {1..$nbJoueurs}
     do
-        tourDeJeu $joueur
+        tourDeJeu "Player$j"
     done
 
     maximum=$(cat etatJeu.txt | tr -s ' ' '\n' | grep -v Player* | sort -rn | head --lines=1)
     joueurGagnant=$(cat etatJeu.txt | grep $maximum | cut -d ' ' -f 1)
-    $"{$JoueurGagnant}_score"=$(($"{$JoueurGagnant}_score"+1))
-    #cat etatJeu.txt >> $(cat etatJeu.txt | tr -s ' ' '\n' | grep -v Player*)
+    numeroGagnant=${JoueurGagnant:6:1}
+    score[$numeroGagnant]=$((${score[$numeroGagnant]}+1))
 done
