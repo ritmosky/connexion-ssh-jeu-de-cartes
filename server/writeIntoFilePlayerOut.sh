dec94839d9652d8b124e2e8c5c3608c112187ab0
luongjim <jimmy.luong@etu.utc.fr>
2022-01-28 11:53:34 +0100

    Ajout script pour que le client joue sa carte

--- server/writeIntoFilePlayerOut.sh
+++ server/writeIntoFilePlayerOut.sh
@@ -2,3 +2,15 @@
 # ce fichier permet au joueur de jouer sa carte et rediriger sa commande vers un fichier qui
 #sera lu par le serveur
 
+
+nomPlayer=$(whoami)
+echo $nomPlayer
+
+if grep -Fxq "$1" listeCartes.txt
+then
+	echo "$1" > /home/jimmy/jeu_cartes_ssh/server/temp/"$nomPlayer"Out
+	echo "Ok vous avez jouer $1"
+else
+	echo "Ce choix n est pas valide, recommencez : "
+fi
+ 
