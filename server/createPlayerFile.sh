af921d1a20bab588b03d418bdc0a6c0201ab8b37
luongjim <jimmy.luong@etu.utc.fr>
2022-01-28 12:18:26 +0100

    Modif createPlayer

--- server/createPlayerFile.sh
+++ server/createPlayerFile.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-rm /home/jimmy/jeu_cartes_ssh/server/temp/playerOut*
-rm /home/jimmy/jeu_cartes_ssh/server/temp/playerIn*
-
+#rm /home/jimmy/jeu_cartes_ssh/server/temp/player*
+file=$(cat "temp/jimmyOut")
+echo $file
 
 nbplayer=$1
 
 for i in $(seq 1 $nbplayer)
 do
 	echo $i
-	touch /home/jimmy/jeu_cartes_ssh/server/temp/player"$i"Out
-	touch /home/jimmy/jeu_cartes_ssh/server/temp/player"$i"In
+	touch /home/jimmy/jeu_cartes_ssh/server/temp/ player"$i"Out
+	touch /home/jimmy/jeu_cartes_ssh/server/temp/ player"$i"In
 done
