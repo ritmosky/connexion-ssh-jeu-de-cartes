8ded8500fa32c58d8d6bd4f9d097589ec5051b05
monemail <monemail@cequevousvoulez.com>
2022-01-28 10:41:24 +0100

    server.sh est fonctionnel

--- server/server.sh
+++ server/server.sh
@@ -2,9 +2,9 @@
 
 script_name=$0
 
-if [ $(whoami) = "root" ]
+if [ $(whoami) = "root" ]
 then
-    
+    echo "Script executé en tant que root."
 else
     echo "Relancer le script avec sudo: sudo $script_name"
 fi
